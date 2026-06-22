12dbe8426f2907f8ed6fdad515fb3d2664da7227
Naplon <94738563+naplon74@users.noreply.github.com>
2026-06-22 17:49:55 +0200

    Add files via upload

---
+++ brave-debloat-linux.sh
@@ -0,0 +1,11 @@
+echo "Brave Linux debloat by Naplon_"
+echo "Github: https://github.com/naplon74/brave-linux-debloat"
+
+sudo mkdir /etc/brave/policies/managed -p
+
+wget https://github.com/naplon74/brave-linux-debloat/releases/tag/v1.0/policies.json
+sudo cp policies.json /etc/brave/policies/managed
+rm policies.json
+
+echo "Brave Linux debloat succceded"
+echo "Edit the policies in /etc/brave/policies/managed"
