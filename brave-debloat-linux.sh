fc3aeb02c38e8bacaa34188b86ace541f6fc2b9b
Naplon <94738563+naplon74@users.noreply.github.com>
2026-06-25 19:36:13 +0200

    Update brave-debloat-linux.sh

--- brave-debloat-linux.sh
+++ brave-debloat-linux.sh
@@ -1,11 +1,73 @@
-echo "Brave Linux debloat by Naplon_"
-echo "Github: https://github.com/naplon74/brave-linux-debloat"
+#!/bin/bash
 
-sudo mkdir /etc/brave/policies/managed -p
+debloat() {
+    echo "Brave Linux debloat by Naplon_"
+    echo "Github: https://github.com/naplon74/brave-linux-debloat"
 
-wget https://github.com/naplon74/brave-linux-debloat/releases/tag/v1.0/policies.json
-sudo cp policies.json /etc/brave/policies/managed
-rm policies.json
+    sudo mkdir /etc/brave/policies/managed -p
 
-echo "Brave Linux debloat succeeded"
-echo "Edit the policies in /etc/brave/policies/managed"
+    wget https://raw.githubusercontent.com/naplon74/brave-linux-debloat/refs/heads/main/policies.json
+    sudo cp policies.json /etc/brave/policies/managed
+    rm policies.json
+
+    echo "Brave Linux debloat succeeded"
+    echo "Edit the policies in /etc/brave/policies/managed"
+}
+
+edit_config() {
+    echo "Editing config file..."
+    sudo nano /etc/brave/policies/managed/policies.json
+}
+
+uninstall() {
+    echo "Uninstalling debloat config file..."
+    sudo rm /etc/brave/policies/managed/policies.json
+    echo "Debloat config file uninstalled."
+}
+
+pause() {
+    echo
+    read -n 1 -s -r -p "Press any key to return to the menu..."
+    clear
+}
+
+
+while true; do
+    echo "▒▓███████▓▒░░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░      ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░░▒▓█▓▒░      ░▒▓██████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░"
+    echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░"
+    echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒▒▓█▓▒░░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░"    
+    echo "░▒▓███████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░░▒▓█▓▒▒▓█▓▒░░▒▓██████▓▒░        ░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░ ░▒▓███████▓▒░░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ ░▒▓█▓▒░"    
+    echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▓█▓▒░ ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░"    
+    echo "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▓█▓▒░ ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░"    
+    echo "░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓██▓▒░  ░▒▓████████▓▒░      ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░░▒▓████████▓▒░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░"                                                                                                                                                               
+    echo                                                                                                                                                                   
+
+    PS3="Brave Linux debloat by Naplon_ - Choose an option: "
+    options=("Debloat Brave Linux" "Edit config file" "Uninstall the debloat config file" "Exit")
+
+    select choix in "${options[@]}"; do
+        case $REPLY in
+            1)
+                debloat
+                pause
+                break
+                ;;  
+            2)
+                edit_config
+                pause
+                break
+                ;;
+            3)
+                uninstall
+                pause
+                break
+                ;;
+            4)
+                exit 0
+                ;;
+            *)
+                echo "Invalid option. Please choose a valid option."
+                ;;
+        esac
+    done
+done 
